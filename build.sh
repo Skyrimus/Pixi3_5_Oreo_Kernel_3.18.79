0037ae39cd3da5313da30d98d210dba02565e2c3
Skyrimus <Skyrimus@yandex.ru>
2018-06-20 17:09:37 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ export CROSS_COMPILE=/root/linaro_6.3/bin/arm-linux-gnueabihf-
 export USE_CCACHE=1
 export ARCH=arm ARCH_MTK_PLATFORM=mt6580
 export TARGET=out
-make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE k80_bsp_defconfig
+make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE pixi3_5_defconfig
 make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE -j4
 
